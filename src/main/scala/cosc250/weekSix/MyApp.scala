7a3117cdcf16abd1305456ea3d25d4f9a2fc1569
William Billingsley <wbilling@une.edu.au>
2016-05-16 16:25:05 +1000

    added solution, might be scrappy (commit in lecture)

--- src/main/scala/cosc250/weekSix/MyApp.scala
+++ src/main/scala/cosc250/weekSix/MyApp.scala
@@ -4,21 +4,27 @@ import java.util.concurrent.TimeoutException
 
 import akka.NotUsed
 import akka.actor._
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Flow, Source}
 import play.api.libs.ws.ahc.AhcWSClient
 import play.api.libs.ws.ning.NingWSClient
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 object MyApp extends App {
 
   import Exercise._
 
+  implicit val system = ActorSystem("exercise")
+  implicit val materializer = ActorMaterializer()
+
 
   // Here's the start of your mission
   val s:Source[Int, NotUsed] = Source(1 to 1000000)
+
+
 
   /*
    * Next, create a flow that will take a Source[Int, NotUsed] and apply a "Fizz Buzz" conversion to it ...
@@ -30,12 +36,38 @@ object MyApp extends App {
    * ... oh, and toString them...
    *
    */
+  val flow = Flow[Int].map[String]({
+    case x if x % 15 == 0 => "FizzBuzz"
+    case x if x % 3 == 0 => "Fizz"
+    case x if x % 5 == 0 => "Buzz"
+    case x => x.toString
+  })
 
+  val slow = Flow[String].map[String]({ x =>
+    try {
+      Thread.sleep(200)
+    } catch {
+      case x:Exception => //
+    }
+    x
+  })
+
+  val t = 1.second
 
   /*
    * Once you have your flow, it's time to materialise it.
    * Let's just stream it to standard out
    */
+  val mid = s.via(flow)
+
+  val withHose = mid.via(slow)
+
+  val withSink = withHose.runForeach(println)
+
+  val after = withSink.andThen({ case _ => system.terminate() })
+
+
+
 
   /*
    * And now let's play with backpressure.
